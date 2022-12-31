fc39bc887640b66fdcc4d4b5cc0576434c105ff3
Joy Fan <chandler0204@gmail.com>
2022-12-31 17:35:42 +0800

    import formik and yup

--- src/components/AddEventDialog.tsx
+++ src/components/AddEventDialog.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -6,20 +6,46 @@ import {
   DialogContentText,
   DialogActions,
   Button,
-} from '@mui/material'
+  TextField,
+} from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const AddEventDialog = ({
   nowEditingDay,
   handleDialogClose,
 }: {
-  nowEditingDay: number | null
-  handleDialogClose: () => void
+  nowEditingDay: number | null;
+  handleDialogClose: () => void;
 }) => {
+  const validationSchema = yup.object({
+    // email: yup
+    //   .string("Enter your email")
+    //   .email("Enter a valid email")
+    //   .required("Email is required"),
+    // password: yup
+    //   .string("Enter your password")
+    //   .min(8, "Password should be of minimum 8 characters length")
+    //   .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      // email: "foobar@example.com",
+      // password: "foobar",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Dialog open={nowEditingDay != null} onClose={handleDialogClose}>
       <DialogTitle>Add New Event</DialogTitle>
+      <DialogContent></DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default AddEventDialog
+export default AddEventDialog;
--- src/type/type.ts
+++ src/type/type.ts
@@ -1,24 +1,38 @@
-import { getEveryDateInMonth } from '../utils/generateDates'
+import { getEveryDateInMonth } from "../utils/generateDates";
 
 export interface EventType {
-    priority: 'High' | 'Medium' | 'Low', title: string, time: number
+  priority: "High" | "Medium" | "Low";
+  title: string;
+  time: number;
 }
 
 export interface DailyDataType {
-    date: number,
-    dayInWeek: number,
-    events: EventType[]
+  date: number;
+  dayInWeek: number;
+  events: EventType[];
 }
 
 export const generateCalenderData = (year: number, month: number) => {
-    let eventData: { date: number, events: EventType }[] = [];
-    if (localStorage.getItem('calender-data') !== null) {
-        eventData = JSON.parse(localStorage.getItem('calender-data') as string)
-    }
+  let eventData: { date: number; events: EventType[] }[] = [];
 
-    return getEveryDateInMonth(year, month).map(dateObj => ({
-        date: dateObj.unix(),
-        dayInWeek: dateObj.day(),
-        events: eventData.filter(event => event.date === dateObj.unix()).map(event => event.events)
-    }))
-}
+  const monthlyData: DailyDataType[] = getEveryDateInMonth(year, month).map(
+    (dateObj) => ({
+      date: dateObj.unix(),
+      dayInWeek: dateObj.day(),
+      events: [],
+    })
+  );
+
+  if (localStorage.getItem("calender-data") !== null) {
+    eventData = JSON.parse(localStorage.getItem("calender-data") as string);
+    eventData.forEach((dailyEvent) => {
+      const thisDay = monthlyData.find(
+        (dailyData) => dailyData.date === dailyEvent.date
+      );
+      if (thisDay) {
+        thisDay.events = dailyEvent.events;
+      }
+    });
+  }
+  return monthlyData;
+};
